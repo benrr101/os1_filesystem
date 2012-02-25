1306d5a0c7759b0d9fc9baefbeb97179cca8ca0b
Benjamin Russell <benrr101@csh.rit.edu>
2012-02-25 16:37:29 -0500

    Adding comments to fsinit

--- fsinit.c
+++ fsinit.c
@@ -20,6 +20,10 @@
 
 // FUNCTIONS ///////////////////////////////////////////////////////////////
 
+/**
+ * Writes null bytes to the maximum size of the filesystem.
+ * @param	UINT	size	Mamimum size of the filesystem in bytes
+ */
 void allocateFS(UINT size) {
 	// Seek to the beginning of the filesystem
 	fseek(fsFile, 0, SEEK_SET);
@@ -33,6 +37,12 @@ void allocateFS(UINT size) {
         }
 }
 
+/**
+ * Create the filesystem. Promps the user for information about the filesystem.
+ * Creates the boot record, initializes the fat table, allocates the full size
+ * of the fs, and initializes 1 cluster of directory table.
+ * @param	char*	name	Name of the filesystem
+ */
 void createFS(char *name) {
 	// Prompt the user for the maximum FS size
 	printf("Enter the maximum size for this filesystem in MB [10]: ");
@@ -111,6 +121,9 @@ void createFS(char *name) {
 	flushBootRecord();
 }
 
+/**
+ * Writes the boot record to the filesystem file
+ */
 void flushBootRecord(void) {
 	// Seek to the beginning of the filesystem file
 	fseek(fsFile, 0, SEEK_SET);
@@ -127,6 +140,11 @@ void flushBootRecord(void) {
 	}
 }
 
+/**
+ * Initializes a cluster for use as a directory table by creating a cluster's
+ * worth of directory table entries.
+ * @param	FSPTR	addr	The cluster to initialize as a directory table
+ */
 void initDirTableCluster(FSPTR addr) {
 	// Calculate the correct offset into the filesystem and seek to it
 	UINT offset = calcOffset(addr);
@@ -150,6 +168,12 @@ void initDirTableCluster(FSPTR addr) {
 	}
 }
 
+/**
+ * Initializes a cluster for use as the fat table. Starts all clusters off as
+ * available with the exception of the fat table, directory table, and boot
+ * record clusters.
+ * @param	FSPTR	addr	Cluster to initialize as the fat table
+ */
 void initFAT(FSPTR addr) {
 	// Calculate the correct offset into the filesystem and seek to it
 	UINT offset = calcOffset(addr);
@@ -176,6 +200,10 @@ void initFAT(FSPTR addr) {
 	}
 }
 
+/**
+ * Loads the boot record information into the global boot record struct.
+ * @param	char*	name	The name of the filesystem to load
+ */
 void loadFS(char *name) {
 	// Load the file from the disk
 	fsFile = fopen(name, "r+");
--- fsinit.h
+++ fsinit.h
@@ -13,10 +13,45 @@
 #define OS1FS_init
 
 // FUNCTIONS ///////////////////////////////////////////////////////////////
+
+/**
+ * Writes null bytes to the maximum size of the filesystem.
+ * @param	UINT	size	Mamimum size of the filesystem in bytes
+ */
+void allocateFS(UINT size);
+
+/**
+ * Create the filesystem. Promps the user for information about the filesystem.
+ * Creates the boot record, initializes the fat table, allocates the full size
+ * of the fs, and initializes 1 cluster of directory table.
+ * @param	char*	name	Name of the filesystem
+ */
 void createFileSystem(char *name);
+
+/**
+ * Writes the boot record to the filesystem file
+ */
 void flushBootRecord(void);
+
+/**
+ * Initializes a cluster for use as a directory table by creating a cluster's
+ * worth of directory table entries.
+ * @param	FSPTR	addr	The cluster to initialize as a directory table
+ */
 void initDirTableCluster(FSPTR addr);
+
+/**
+ * Initializes a cluster for use as the fat table. Starts all clusters off as
+ * available with the exception of the fat table, directory table, and boot
+ * record clusters.
+ * @param	FSPTR	addr	Cluster to initialize as the fat table
+ */
 void initFAT(FSPTR addr);
+
+/**
+ * Loads the boot record information into the global boot record struct.
+ * @param	char*	name	The name of the filesystem to load
+ */
 void loadFileSystem(char *name);
 
 #endif
