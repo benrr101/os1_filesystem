e1f3caf6da8147178b20cb3a7467b7ce980663fb
Benjamin Russell <benrr101@csh.rit.edu>
2012-02-25 13:06:27 -0500

    Comments for fswrite.c/h

--- fswrite.c
+++ fswrite.c
@@ -19,6 +19,12 @@
 #include "fsread.h"
 
 // FUNCTIONS ///////////////////////////////////////////////////////////////
+/**
+ * Allocates an additional cluster to add to the chain of the current file.
+ * Also writes the update to the FAT.
+ * @param	FSPTR	currentCluster	The current end cluster of the file chain
+ * @return	FSPTR	The cluster index of the newly allocated cluster
+ */
 FSPTR allocateCluster(FSPTR currentCluster) {
 	// Store the current location in the FS file
 	UINT curPos = ftell(fsFile);
@@ -46,6 +52,12 @@ FSPTR allocateCluster(FSPTR currentCluster) {
 	return cluster;
 }
 
+/**
+ * Creates a file with the given name. Initializes the directory table entry
+ * for the file and allocates 1 cluster for the file.
+ * @param	char[112]	name	The name of the file
+ * @return	FSPTR		The index of the first cluster of the file
+ */
 FSPTR createFile(char name[112]) {
 	// Find the next free directory entry and FAT entry
 	UINT freeDir = getFirstFreeDirEntry();
@@ -71,6 +83,11 @@ FSPTR createFile(char name[112]) {
 	return clusterIndex;
 }
 
+/**
+ * Removes the specified file from the filesystem. It also unallocates the
+ * chain of clusters.
+ * @param	char[112]	name	The name of the file to delete
+ */
 void removeFile(char name[112]) {
 	// Find the file in the directory table
 	UINT dirAddr = getDirTableAddressByName(name);
@@ -108,6 +125,11 @@ void removeFile(char name[112]) {
 	} while(entry != FAT_EOC);
 }
 
+/**
+ * Writes the specified value to the specified ADDRESS of an entry in the FAT.
+ * @param	UINT	index	The address of the FAT table entry to write to
+ * @param	FatEntry	value	The value to write to the FAT entry.
+ */
 void writeToFAT(UINT index, FatEntry value) {
 	// Seek to the index of the FAT
 	fseek(fsFile, index, SEEK_SET);
--- fswrite.h
+++ fswrite.h
@@ -10,11 +10,36 @@
 #ifndef OS1FS_write
 #define OS1FS_write
 
-// INCLUDES ////////////////////////////////////////////////////////////////
-
 // FUNCTIONS ///////////////////////////////////////////////////////////////
 
+/**
+ * Allocates an additional cluster to add to the chain of the current file.
+ * Also writes the update to the FAT.
+ * @param	FSPTR	currentCluster	The current end cluster of the file chain
+ * @return	FSPTR	The cluster index of the newly allocated cluster
+ */
+FSPTR allocateCluster(FSPTR currentCluster);
+
+/**
+ * Creates a file with the given name. Initializes the directory table entry
+ * for the file and allocates 1 cluster for the file.
+ * @param	char[112]	name	The name of the file
+ * @return	FSPTR		The index of the first cluster of the file
+ */
 FSPTR createFile(char name[112]);
+
+/**
+ * Removes the specified file from the filesystem. It also unallocates the
+ * chain of clusters.
+ * @param	char[112]	name	The name of the file to delete
+ */
+void removeFile(char name[112]);
+
+/**
+ * Writes the specified value to the specified ADDRESS of an entry in the FAT.
+ * @param	UINT	index	The address of the FAT table entry to write to
+ * @param	FatEntry	value	The value to write to the FAT entry.
+ */
 void writeToFAT(UINT index, FatEntry value);
 
 #endif
